222bfaced6a019c491183ce53e2f99f9b4521fcb
Larry House <dotnet.ninja@outlook.com>
2022-01-21 21:49:42 -0600

    Remove unused controller actions, models & views

--- src/DotNetNinja.HelloK8S/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace DotNetNinja.HelloK8S.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
